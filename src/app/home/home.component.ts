6c55441347a8b6e96dfcf64e8c6b5453905b0c90
Emircan <emircandv787@gmail.com>
2024-07-12 11:11:54 +0300

    x

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Input } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent {
+
+ @Input() name:String= "emircan";
+
+}
