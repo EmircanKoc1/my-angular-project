18e6d9956c96c05abd08429efaf440c43feba32a
Emircan <emircandv787@gmail.com>
2024-07-12 11:48:32 +0300

    angular proj

--- src/app/directives/example.directive.ts
+++ src/app/directives/example.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appExample]'
 })
 export class ExampleDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+    element.nativeElement.style.backgroundColor="red";
+    
+  }
 
 }
